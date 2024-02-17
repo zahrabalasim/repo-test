17769d2990dd8bad76ed594695f61b50d995d4f7
Zahra Balasim <154760965+zahrabalasim@users.noreply.github.com>
2024-02-17 16:10:16 +0300

    solve the task of objest

---
+++ index.js
@@ -0,0 +1,22 @@
+// class SCT_friends {
+//     constructor ( Name , Job, Age, Nationality){
+//         this.Name= Name;
+//         this.Job= Job;
+//         this.Age=Age;
+//         this.Nationality= Nationality;
+//     }
+//     myFavFriend (){
+//         console.log(' this is my favourite friend '+ this.Name + ' her age ' + this.Age + ' her Job ' + this.Job + ' her Natianality ' + this.Nationality);
+//     }
+// }
+// let Aya = new SCT_friends ('Aya', 'Teacher', 24 , 'Iraqi');
+// Aya.myFavFriend();
+///////////////////////////
+
+var friend = {
+    print: function() {
+    console.log(this.name); }
+    
+}
+ 
+
